f7fa862af881d0a046a2d6807b0a94b73f29bdf4
Tania Savitri <soxtania@gmail.com>
2022-10-08 12:25:05 +0200

    added flair for sentiment

--- part_2/airflow-stream/stream-to-pubsub.py
+++ part_2/airflow-stream/stream-to-pubsub.py
@@ -1,4 +1,4 @@
-import base64, json, tweepy, os, functions_framework
+import base64, json, tweepy, os, flair
 from google.cloud import pubsub_v1
 
 '''
@@ -6,7 +6,7 @@ CONSTANT
 '''
 project_id=os.getenv('GOOGLE_CLOUD_PROJECT')
 bearer=os.getenv('bearer')
-topic_id = 'pubsub-stream-filter'
+topic_id = 'pubsub-stream-filter1'
 
 def write_to_pubsub(data, stream_rule):
     data["stream_rule"] = stream_rule
@@ -31,18 +31,20 @@ class Client(tweepy.StreamingClient):
     def on_response(self, response):
         tweet_data = response.data.data
         user_data = response.includes['users'][0].data
-        metrics_fiels = response.data.public_metrics
+        # metrics_fiels = response.data.public_metrics
         result = tweet_data
         result["user"] = user_data
-        result['metric'] = metrics_fiels
+        # result['metric'] = metrics_fiels
 
         write_to_pubsub(result, self.stream_rule)
 
-@functions_framework.cloud_event
-def hello_pubsub(cloud_event):
-    stream_rule = base64.b64decode(cloud_event.data["message"]["data"]).decode('UTF-8')
+# @functions_framework.cloud_event
+# def hello_pubsub(cloud_event):
+
+if __name__ == "__main__":
+    stream_rule = 'norge'#base64.b64decode(cloud_event.data["message"]["data"]).decode('UTF-8')
     tweet_fields = ['id', 'text', 'author_id', 'created_at','public_metrics']
-    metrics_fiels = ['retweet_count','reply_count','like_count','quote_count']
+    # metrics_fiels = ['retweet_count','reply_count','like_count','quote_count']
     user_fields = ['id','username']
     expansions = ['author_id']
 
@@ -57,4 +59,4 @@ def hello_pubsub(cloud_event):
     # add new rules and run stream
     streaming_client.add_rules(tweepy.StreamRule(stream_rule))
     streaming_client.filter(tweet_fields=tweet_fields, expansions=expansions, user_fields=user_fields)
-    return f"Streaming for keyword: {stream_rule}"
+    # return f"Streaming for keyword: {stream_rule}"
